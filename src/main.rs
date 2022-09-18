20085d20e8bb526b16d8d7bcae470bba194e348a
rektdeckard <friedtm@gmail.com>
2022-09-17 18:45:35 -0600

    feat(app): flag to create new

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,33 @@ fn main() -> Result<(), Box<dyn Error>> {
                 .long("file")
                 .help("Load json from Sectors Without Number")
                 .takes_value(true)
+                .value_name("FILE")
                 .overrides_with("new"),
+        )
+        .arg(
+            Arg::with_name("new")
+            .short("n")
+            .long("new")
+            .help("Create a random sector")
+            .takes_value(true)
+            .value_name("FILE")
+            .overrides_with("file"),
         )
         .get_matches();
 
-    let filename = matches.value_of("file").expect("No file specified");
-    println!("{}", filename);
-    let file = File::open(filename).expect("Could not open file");
-    let world: model::World = from_reader(file).expect("Could not read json");
-    let mut app = app::App::new(world);
+    let mut app: app::App;
+
+    if let Some(filename) = matches.value_of("file") {
+        let file = File::open(filename).expect("Could not open file");
+        let world: model::World = from_reader(file).expect("Could not read json");
+        app = app::App::new(world);
+    } else if let Some(filename) = matches.value_of("new") {
+        todo!();
+        let _file = File::create(filename)?;
+    } else {
+        panic!("Must open or create a sector");
+    }
+    
 
     ui::init(&mut app)
 }
